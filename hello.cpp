c95582c4412efe70c4a79ab581fe795cc3911dd5
Lindis0804 <hieuledinh0804@gmail.com>
2023-06-02 19:47:04 +0700

    add report for lab6

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-  // int s[10] = {1,2,3,4};
-  // char st[10] = {'1','2','3'};
-  // cout<<sizeof(s)<<sizeof(st)<<" "<<s[5]<<endl;
-  system("hello > out.txt");
-  return 0;
-}
